53d3f6d0f932afb8735abaf695605ad9c71dce4e
yashjain-10 <yashshubh2000@gmail.com>
2023-02-06 15:02:29 +0000

    6/2/23

---
+++ SudokuGame/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  SudokuGame
+//
+//  Created by Yash Jain on 2/6/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SudokuGame/HomeViewController.m
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.m
+//  SudokuGame
+//
+//  Created by Yash Jain on 2/6/23.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+@end
